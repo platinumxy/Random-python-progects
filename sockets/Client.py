b57d20ac575b3b37ec8afe8f7648f9e31a2d0d1b
platinumxy <83834735+platinumxy@users.noreply.github.com>
2022-10-20 17:53:08 +0100

    Updated the Server and Client
    Modified the Server to be able to handle multiple users who can all send their own messages.
    And modified the client to be easier to use

--- sockets/Client.py
+++ sockets/Client.py
@@ -1,22 +1,4 @@
 import socket
-import os
-
-def MoveTo(fileName:str) ->  str: return __RecursiveMoveTo(fileName, True)
-def __RecursiveMoveTo(fileName, __START=False):
-    cwd = os.getcwd()
-    for file in os.listdir(cwd):
-        if os.path.isfile(file): 
-            if file == fileName:return cwd
-        else :
-            try :
-                os.chdir(cwd+"\\"+file)
-                found = __RecursiveMoveTo(fileName)
-                if found : return cwd 
-            except NotADirectoryError: pass 
-    if __START :raise FileNotFoundError()
-    else: return False 
-
-MoveTo("textToSend.txt")
 
 s=socket.socket()
 
@@ -24,11 +6,15 @@ host = input("Enter host name (default will be local machine)")
 if host == "":
     host=socket.gethostname() #server hostname
 port=62_444 #same as server
-
-
 s.connect((host,port))
-fileToSend = open("textToSend.txt","r")
-content = fileToSend.read()
-s.send(content.encode())
+
+
+while True :
+    message = input("Enter the message you wish to send :\n")
+    if message == "QUIT": break
+    try :
+        s.send(message.encode())
+    except ConnectionResetError: 
+        input("Connection was reset restart the program to try reconect")
+        break
 s.close()
-input("Sent!")
--- sockets/Server.py
+++ sockets/Server.py
@@ -1,17 +1,34 @@
 import socket
+import threading
 
-s=socket.socket()
-host=socket.gethostname()
+
+def clientHandeler(c, addr, index):
+    while True:
+        try:
+            content = c.recv(300).decode()
+            if not content:
+                break
+            print(f"{addr[0]} -> {addr[1]} : {content}")
+        except ConnectionResetError:
+            break
+    clients.pop(index)
+    print("Client connected", addr)
+    return
+
+
+clients = []
+
+s = socket.socket()
+host = socket.gethostname()
 print(host)
-port=62_444 
-s.bind((host,port))
-s.listen(10)
+port = 62_444
+s.bind((host, port))
+s.listen(0)
 while True:
-    c,addr=s.accept()
-    print ("Client connected",addr)
-    print ("Got Connection from" ,addr)
-    content=c.recv(100).decode()
-    if not content:
-        break
-    print (content)
-print("Resived")
+    c, addr = s.accept()
+    print("Client connected", addr)
+    clients.append((threading.Thread(target=clientHandeler, args=(
+        c, addr, len(clients))), c, addr, len(clients)))
+    clients[-1][0].start()
+
+
