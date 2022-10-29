b18b9597ccbe2857d59bab6ec503d82f5776953a
platinumxy <83834735+platinumxy@users.noreply.github.com>
2022-10-30 00:02:01 +0100

    Improved the timeFunction
    Used def rather than class

--- Decoratorators/timer.py
+++ Decoratorators/timer.py
@@ -1,10 +1,7 @@
-class timeFunction:
-    def __init__(self, function):
-        from time import time
-        self.func = function
-        self.time = time
-
-    def __call__(self, *args, **kwargs):
-        startTime = self.time()
-        self.func(*args, **kwargs)
-        print(f"{self.func.__name__} took - {self.time()-startTime}")
+def timeFunction(function):
+    from time import time 
+    def __Timer(*args, **kwargs) :
+        startTime = time()
+        function(*args, **kwargs)
+        print(f"{function.__name__} took - {time() - startTime} seconds") 
+    return __Timer
