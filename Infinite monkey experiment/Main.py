57cae62874d304408b0b1951f3aa6f0bb71eb6c0
platinumxy <83834735+platinumxy@users.noreply.github.com>
2022-11-15 23:58:56 +0000

    Created the home page for IME
    Created the MainWindow class, the home page and started the refresher function

--- Infinite monkey experiment/Main.py
+++ Infinite monkey experiment/Main.py
@@ -1,8 +1,10 @@
 import tkinter.messagebox as mb
 import tkinter as tk
 import tkinter.ttk as ttk
-from time import time
+from time import sleep , time
+from math import floor 
 from os import _exit
+from datetime import datetime , timedelta
 from threading import Thread
 from dataclasses import dataclass, field
 from functools import cache
@@ -47,13 +49,16 @@ class monkey:
                 
             if not endlesslyGuess:
                 break
+            
+            #To force the thread to give up the GIL
+            sleep(0.00001)
 
     def addWordToWords(self, word) -> None:
         if not self.noPrint:
             print(self.name, "found", word)
         self.words.append(word)
         self.numWords = self.numWords + 1
-        if len(word) >= len(self.largestWord):
+        if len(word) > self.lenLargestWord:
             self.largestWord = word
             self.lenLargestWord = len(word)
 
@@ -113,6 +118,99 @@ class HowManyMonkeys:
         self.mainWin.mainloop()
         
 
+class MainWindowIME(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.startTime = time()
+        self.timeSinceStart = 0
+        
+        self.titleFont = ('Arial', 20, 'bold')
+        self.style = ttk.Style(self)
+        self.style.configure('TLabel', font=('Arial', 11))
+        self.style.configure('TButton', font=('Arial', 11))
+        self.LabPaddy = {"padx":1, "pady":5}
+        self.ArrowPaddy = {"pady":5}
+        
+        self._frame = None 
+        self._currentFrameFunc = None 
+        self._travelHis: list[function] = [] 
+        
+        
+        self.protocol("WM_DELETE_WINDOW", self._exit)
+        self.title('Infinite monkey experiment')
+        self.geometry('700x500')
+        self.swapFrame(self.renderHomePage)
+        self._refresh()
+        
+        
+    def _refresh(self):
+        
+        #Important note all variables that might need updating start with a capital to differenchiate 
+        if self._currentFrameFunc == self.renderHomePage:
+            self.NumWordsFound.config(text=str(numWords()))
+            self.LargestWordFound.config(text=largestWord())
+            
+        self.timeSinceStart = time() - self.startTime
+        self.TimeSSLabel.config(text=str(floor(self.timeSinceStart)))
+        self.after(100, self._refresh)
+
+    def _exit(self):
+        _exit(0)
+
+    def swapFrame(self, render,*, _dontAddToBackLog=False, refresh=False) -> None :
+        if self._currentFrameFunc == render and not refresh:
+            pass
+        if self._frame != None :
+            self._frame.destroy()
+            if not _dontAddToBackLog :
+                self._travelHis.append(self._currentFrameFunc)
+        self._currentFrameFunc = render
+        self._frame = ttk.Frame()
+        self._frame.pack()
+        render()
+
+    def renderHomePage(self):
+        ttk.Label(self._frame, text="The Infinite Monkey Experiment", font=self.titleFont).grid(row=0, column=0, columnspan=3, pady=20)
+        
+        
+        ttk.Label(self._frame, text="Number of monkeys").grid(row=1, column=0, pady=10)
+        ttk.Label(self._frame, text= "➤").grid(row=1, column=1, pady=10)
+        ttk.Label(self._frame, text=str(len(monkeys))).grid(row=1, column=2, padx=self.LabPaddy["padx"], pady=10)
+        
+        ttk.Label(self._frame, text="Num words found").grid(row=2, column=0, **self.ArrowPaddy)
+        ttk.Label(self._frame, text= "➤").grid(row=2, column=1, **self.ArrowPaddy)
+        self.NumWordsFound = ttk.Label(self._frame)
+        self.NumWordsFound.grid(row=2, column=2, **self.LabPaddy)
+        
+        ttk.Label(self._frame, text="Largest word found").grid(row=3, column=0, **self.ArrowPaddy)
+        ttk.Label(self._frame, text= "➤").grid(row=3, column=1, **self.ArrowPaddy)
+        self.LargestWordFound = ttk.Label(self._frame)
+        self.LargestWordFound.grid(row=3, column=2, **self.LabPaddy)
+
+        
+        self.BackHomeTimeSinceStart(row=4, columnSpan=3)
+        
+    def BackHomeTimeSinceStart(self, *, row, column=0, columnSpan=5): 
+        self.Home = ttk.Button(self._frame, text="Home", command=lambda: self.swapFrame(self.renderHomePage))
+        self.Back = ttk.Button(self._frame, text="Back", command=lambda: self.swapFrame(self._travelHis.pop(), _dontAddToBackLog=True))
+        if columnSpan%2 == 0 :
+            HCS = int(columnSpan/2)
+            self.Home.grid(row=row, column=column, columnspan=HCS)
+            self.Back.grid(row=row, column=column+HCS, columnspan=HCS)
+        else : 
+            HCS = floor(columnSpan/2)
+            self.Home.grid(row=row, column=column, columnspan=HCS)
+            self.Back.grid(row=row, column=column+1+HCS, columnspan=HCS)
+        
+        
+        
+        timeSS = ttk.Labelframe(self._frame, labelwidget=ttk.Label(text="Seconds Since Start", font=('Arial', 13)))
+        timeSS.grid(row=row+1,column=column, columnspan=columnSpan)
+        
+        self.TimeSSLabel = ttk.Label(timeSS, text=str(self.timeSinceStart))
+        self.TimeSSLabel.pack()
+    
+    
 @cache
 def currentwInALLW(word: str) -> bool:
     if not (2 < len(word) < 10):
@@ -120,13 +218,32 @@ def currentwInALLW(word: str) -> bool:
     return word[:-1] in ALLWORDS
 
 
+def numWords() -> int :
+    total = 0 
+    for monk in monkeys :
+        total = total + monk.numWords
+    return total 
+
+def largestWord() -> str :
+    BigWord =""
+    for monk in monkeys:
+        if monk.lenLargestWord > len(BigWord):
+            BigWord = monk.largestWord
+    return BigWord
+
 def monkeyHandeler(numMonkeys: int, * , noPrint=False) -> None:
-    monkeys: list[monkey] = []
-    monkeyThreads: list[Thread] = []
+    global monkeys, monkeyThreads
+    
+    try: # Creates the variables if they are missing 
+        monkeys; monkeyThreads
+    except:
+        monkeys, monkeyThreads = [], [] 
+    
     for monkeyNum in range(numMonkeys):
         monkeys.append(monkey(f"Monkey - {monkeyNum+1}", noPrint))
         monkeyThreads.append(Thread(target=monkeys[-1].guessWord))
         monkeyThreads[-1].start()
+
 
 
 ALLWORDS = [word for word in ALLWORDS if len(word) > 3 and len(word) < 10]
@@ -140,6 +257,13 @@ for _ in range(SPACEFREQUENCY):
 
 if __name__ == "__main__":
     NUMOFMONKEYS:int = 1
+    #As type annotations cannot be declared as global inside of functions 
+    monkeys: list[monkey] = []
+    monkeyThreads: list[Thread] = []
+    
     HowManyMonkeys().mainloop()
+    Thread(monkeyHandeler(NUMOFMONKEYS)).start()
+    
+    MainWindowIME().mainloop()
 
-    monkeyHandeler(NUMOFMONKEYS)
+    #TODO add a search
